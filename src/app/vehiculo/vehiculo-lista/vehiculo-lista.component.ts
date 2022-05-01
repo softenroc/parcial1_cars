93602286176f1e327b4beb1c6eeef84606b6ca15
Dario Fernando Rojas <df.rojasr1@uniandes.edu.co>
2022-05-01 11:43:15 -0500

    Mostrar el total de vehiculos por marca

--- src/app/vehiculo/vehiculo-lista/vehiculo-lista.component.ts
+++ src/app/vehiculo/vehiculo-lista/vehiculo-lista.component.ts
@@ -9,6 +9,8 @@ import { VehiculoService } from '../vehiculo.service';
 export class VehiculoListaComponent implements OnInit {
 
   vehiculos: Array<Vehiculo> = [];
+  marcas : Array<string> = ["Renault", "Chevrolet", "Nissan"];
+  cantidad : Array<number> = [0,0,0];
 
   constructor(private vehiculoService: VehiculoService) { }
 
@@ -19,10 +21,19 @@ export class VehiculoListaComponent implements OnInit {
   getCourseList(): void {
    this.vehiculoService.getVehiculos().subscribe( vehiculos => {
     this.vehiculos = vehiculos;
+    console.log ("Vehiculos: ", this.vehiculos);
+    this.contarVehiculosPorMarca();
    },   err => {
     this.vehiculos = [];
     console.log("Error al consultar o no existen ")
   });
+  }
+
+  contarVehiculosPorMarca(): void {
+    for (let index = 0; index < this.marcas.length; index++) {
+      this.cantidad[index]= this.vehiculos.filter(vehiculo => vehiculo.marca == this.marcas[index] ).length;
+      console.log("Cantidad y Marca: ", this.cantidad[index], this.marcas[index]);
+    }
   }
 
 }
