facc9f3f2f7669363d0318c40f83f667dad9f3e5
Dario Fernando Rojas <df.rojasr1@uniandes.edu.co>
2022-05-01 09:38:24 -0500

    Crear modulo vehiculo

---
+++ src/app/vehiculo/vehiculo.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { VehiculoComponent } from './vehiculo.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [VehiculoComponent]
+})
+export class VehiculoModule { }
