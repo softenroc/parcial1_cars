fd03b912bc2aaf1fafb07bfb4dd01981a1da8e03
Dario Fernando Rojas <df.rojasr1@uniandes.edu.co>
2022-05-01 10:16:32 -0500

    Actualizar archivo de pruebas de componente

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,18 +16,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'base-project'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('base-project');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'base-project app is running!'
-    );
-  });
 });
