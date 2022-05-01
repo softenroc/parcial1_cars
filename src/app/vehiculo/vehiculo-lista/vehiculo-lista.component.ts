0a485c14325a73d24c5028207c4e494e3ebd1671
Dario Fernando Rojas <df.rojasr1@uniandes.edu.co>
2022-05-01 09:38:35 -0500

    Crear componente vehiculo-lista

---
+++ src/app/vehiculo/vehiculo-lista/vehiculo-lista.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vehiculo-lista',
+  templateUrl: './vehiculo-lista.component.html',
+  styleUrls: ['./vehiculo-lista.component.css']
+})
+export class VehiculoListaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
