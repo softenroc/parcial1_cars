7f3761c76f0cb6d6415187ed515e1d859bd5cbf8
Dario Fernando Rojas <df.rojasr1@uniandes.edu.co>
2022-05-01 10:31:19 -0500

    Mostrar el componente de listar vehiculos en la pagina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import { VehiculoModule } from './vehiculo/vehiculo.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     VehiculoModule
   ],
   providers: [],
--- src/app/vehiculo/vehiculo.module.ts
+++ src/app/vehiculo/vehiculo.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VehiculoComponent } from './vehiculo.component';
+import { VehiculoListaComponent } from './vehiculo-lista/vehiculo-lista.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [VehiculoComponent]
+  exports: [VehiculoListaComponent],
+  declarations: [VehiculoComponent, VehiculoListaComponent]
 })
 export class VehiculoModule { }
