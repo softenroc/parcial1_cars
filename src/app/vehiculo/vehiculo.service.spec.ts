0c6d05072c1b6ac612030d42f4b034e522ef5373
Dario Fernando Rojas <df.rojasr1@uniandes.edu.co>
2022-05-01 19:37:26 -0500

    Adicionar prueba unitaria para servicio vehiculo

--- src/app/vehiculo/vehiculo.service.spec.ts
+++ src/app/vehiculo/vehiculo.service.spec.ts
@@ -1,8 +1,15 @@
+import { Vehiculo } from './vehiculo';
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async, inject } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { VehiculoService } from './vehiculo.service';
+import { faker } from '@faker-js/faker';
+
+let httpTestingController: HttpTestingController;
+let service: VehiculoService;
+var vehiculos: Array<Vehiculo> = [];
+const baseUrl ='https://gist.githubusercontent.com/josejbocanegra/17bb8c76405e43655d551a90800c8a81/raw/d41b4acc3457e51e7533fad6d5e9925ee9676457/202212_MISW4104_Grupo1.json'
 
 describe('Service: Vehiculo', () => {
   beforeEach(() => {
@@ -10,9 +17,45 @@ describe('Service: Vehiculo', () => {
       imports: [HttpClientTestingModule],
       providers: [VehiculoService]
     });
+
+    vehiculos = [];
+
+    for (let i = 0; i < 10; i++) {
+      vehiculos.push(
+        new Vehiculo(
+          faker.datatype.number(),
+          faker.lorem.sentence(),
+          faker.lorem.sentence(),
+          faker.datatype.number()
+        )
+      );
+    }
+
+
+    httpTestingController = TestBed.get(HttpTestingController);
+    service = TestBed.get(VehiculoService);
+
   });
 
   it('should ...', inject([VehiculoService], (service: VehiculoService) => {
     expect(service).toBeTruthy();
   }));
+
+
+  it('if consume getVehiculos returned Observable should match the right data', () => {
+    service.getVehiculos().subscribe((vehiculos) => {
+      expect(vehiculos.length).toEqual(10);
+    });
+
+    const req = httpTestingController.expectOne(baseUrl);
+
+    expect(req.request.method).toEqual('GET');
+    req.flush(vehiculos);
+
+  });
+
+afterEach(() => {
+  httpTestingController.verify();
+});
+
 });
